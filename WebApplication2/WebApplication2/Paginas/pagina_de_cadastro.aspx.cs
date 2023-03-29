86383bd7529511aa65449898e47d89ae5a70787c
LAB201 <LAB201@serveduc.com>
2023-03-28 22:17:07 -0300

    complementar personalização

--- WebApplication2/WebApplication2/Paginas/pagina_de_cadastro.aspx.cs
+++ WebApplication2/WebApplication2/Paginas/pagina_de_cadastro.aspx.cs
@@ -27,54 +27,48 @@ namespace WebApplication2.Paginas
                     rollingInterval: RollingInterval.Hour)
                     .CreateLogger();
 
-                try
+            try
+            {
+                connection.Open();
+
+                if (rdoTipo.SelectedValue == "Cuidador")
                 {
-           
-                    connection.Open();
-                    var comando = new MySqlCommand($@"INSERT INTO cuidadores (Nome, Idade, Email, Senha) 
-                            VALUES ('{txtNome.Text}' ', ' '{txtSobrenome.Text}', '0', '{txtEmail.Text}', '{txtSenha.Text}')", connection);
-                    comando.ExecuteNonQuery();
-                    connection.Close();
-
-                    connection.Open();
-
-                    if (rdoTipo.SelectedValue == "Cuidador")
-                    {
-                            comando = new MySqlCommand($@"INSERT INTO cuidadores (Nome, DataNascimento, Email, Senha, Telefone) 
+                    var comando = new MySqlCommand($@"INSERT INTO cuidadores (Nome, Nascimento, Email, Senha, Telefone) 
                             VALUES ('{txtNome.Text}' ' ' '{txtSobrenome.Text}', '{txtNascimento.Text}', '{txtEmail.Text}', '{txtSenha.Text}', '{txtTelefone.Text}')", connection);
-                            comando.ExecuteNonQuery();
-                    }
+                    comando.ExecuteNonQuery();
+                }
 
                 if (rdoTipo.SelectedValue == "Idoso")
                 {
-                            var comando1 = new MySqlCommand($@"INSERT INTO idosos (Nome, DataNascimento, Email, Senha, Telefone) 
+                    var comando1 = new MySqlCommand($@"INSERT INTO idosos (Nome, Nascimento, Email, Senha, Telefone) 
                         VALUES ('{txtNome.Text}' ' ' '{txtSobrenome.Text}', '{txtNascimento.Text}', '{txtEmail.Text}', '{txtSenha.Text}', '{txtTelefone.Text}')", connection);
-                            comando1.ExecuteNonQuery();
-                    }
-
-                        SiteMaster.ExibirAlert(this, "Perfil cadastrado com sucesso!");
-                        txtNome.Text = "";
-                        txtSobrenome.Text = "";
-                        txtNascimento.Text = "";
-                        txtEmail.Text = "";
-                        txtSenha.Text = "";
-                        txtTelefone.Text = "";
-                        txtConfirmarEmail.Text = "";
-                        txtConfirmarSenha.Text = "";
-
+                    comando1.ExecuteNonQuery();
                 }
 
-                catch (Exception erro)
-                {
-                    Log.Error("Erro ao cadastrar novo usuario" + erro);
-                }
+                SiteMaster.ExibirAlert(this, "Perfil cadastrado com sucesso!");
+                txtNome.Text = "";
+                txtSobrenome.Text = "";
+                txtNascimento.Text = "";
+                txtEmail.Text = "";
+                txtSenha.Text = "";
+                txtTelefone.Text = "";
+                txtConfirmarEmail.Text = "";
+                txtConfirmarSenha.Text = "";
+                connection.Close();
 
-                finally
-                {
-                    connection.Close();
-                }
+            }
 
-            
+            catch (Exception erro)
+            {
+                Log.Error("Erro ao cadastrar novo usuario" + erro);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
 
         }
     }
